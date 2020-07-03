7ea61348d966e2ba80bb8e7f488327600625dae7
Caio César Silva <caiocesarsilva08@gmail.com>
2020-07-02 22:26:13 -0300

    Adicionei o banner na página inicial, agora eu preciso fazer com que a div de cards faça um overlay em cima desse banner.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { CardContainerComponent } from './card-container/card-container.component';
 import { CardComponent } from './card-container/card/card.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent, CardContainerComponent, CardComponent],
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    CardContainerComponent,
+    CardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
